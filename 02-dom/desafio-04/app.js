1831bf457a0408dd7d946df4ffa67049ce53093b
Lucio Flavio C Matias <lucioflavio.cmatias90@hotmail.com>
2020-09-24 07:20:42 -0300

    introducao a condicionais e listas

--- 02-dom/desafio-04/app.js
+++ 02-dom/desafio-04/app.js
@@ -1,14 +1,31 @@
 new Vue({
 	el: '#desafio',
 	data: {
-		aplicarEstilo: true
+		classe1: 'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
+		cor5: '',
+		estilo5: {
+			width: '100px',
+			height: '100px'
+		}
 	},
 	methods: {
 		iniciarEfeito() {
-			//
+			setInterval(() => {
+				this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque';
+			}, 1000);
 		},
 		iniciarProgresso() {
 
+		},
+		setPerigo(event) {
+			if (event.target.value == 'true') {
+				this.perigo = true;
+			} else {
+				this.perigo = false;
+			}
 		}
 	}
 })
