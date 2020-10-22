40a9fb407393478c1f9580d0a34372e216c5f5fd
Lucio Flavio C Matias <lucioflavio.cmatias90@hotmail.com>
2020-10-22 08:01:31 -0300

    componente pai comunicando componente filho

---
+++ 08-comunicacao-com-componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ `${nomeAlternativo} ${inverterNome()}` }}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['nome'],
+    data() {
+        return {
+            nomeAlternativo: this.nome
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
